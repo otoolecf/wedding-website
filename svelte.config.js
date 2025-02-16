69668fc80f26fe5042a2c62fdca3ed6ecba59477
otoolecf <otoolecf@gmail.com>
2025-02-15 19:09:22 -0800

    undo env file changes, env is set on the deploy settings

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,6 @@ const config = {
     // This ensures proper path resolution
     paths: {
       base: ''
-    },
-    platformProxy: {
-      configPath: 'wrangler.toml'
     }
   },
   preprocess: vitePreprocess()
