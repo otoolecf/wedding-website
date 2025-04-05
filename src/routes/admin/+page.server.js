aadf6f9ce9badd652d17381138daad3b4e3e7241
otoolecf <otoolecf@gmail.com>
2025-04-05 12:10:53 -0700

    remove admin dashboard page file

--- src/routes/admin/+page.server.js
+++
@@ -1,6 +0,0 @@
-// src/routes/admin/pages/+page.server.js
-export function load({ params }) {
-  // Return empty props object since we're not loading any specific page data here
-  console.log('admin/pages/+page.server.js: params: ', params);
-  return {};
-}
