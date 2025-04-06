21d0240a54ef10cd08c0aa02ea22a929c17ebf33
otoolecf <otoolecf@gmail.com>
2025-04-05 17:08:21 -0700

    sql migrations / setup

---
+++ src/lib/db/migrations/004_add_default_email_template.sql
@@ -0,0 +1,10 @@
+-- Add default email template
+INSERT INTO email_templates (template) VALUES (
+  '<h2>Thank you for your RSVP!</h2>
+  <p>Here''s a summary of your response:</p>
+  
+  ${form_data}
+  
+  <p>If you need to make any changes to your RSVP, please contact us directly.</p>
+  <p>We look forward to celebrating with you!</p>'
+); 
